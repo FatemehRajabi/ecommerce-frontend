9ac56453cf7e33803812f2d01bdbc208e035cf0c
FatemehRajabi <100088087+FatemehRajabi@users.noreply.github.com>
2025-05-14 10:06:10 -0400

    Create styled login form with Tailwind CSS

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,11 @@
 export default function Login(){
     return(
-        <div>Login Component</div>
+        <div className="flex flex-col">
+            <form className="flex flex-col space-y-4 p-8 max-w-sm mx-auto">
+                <input className="p-2 border border-gray-300 rounded" type="email" placeholder="Email" />
+                <input className="p-2 border border-gray-300 rounded" type="password" placeholder="Password" />
+                <input className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600" type="submit" placeholder="Submit" />
+            </form>
+        </div>
     );
 }
