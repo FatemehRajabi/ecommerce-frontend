d6fa5d8b711ada9f08c5dc403ebeca23da6130f8
FatemehRajabi <100088087+FatemehRajabi@users.noreply.github.com>
2025-05-22 12:08:39 -0400

    Add state and onChange handlers for login form inputs

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
+
 export default function Login(){
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return(
         <div className="flex flex-col">
             <form className="flex flex-col space-y-4 p-8 max-w-sm mx-auto">
-                <input className="p-2 border border-gray-300 rounded" type="email" placeholder="Email" />
-                <input className="p-2 border border-gray-300 rounded" type="password" placeholder="Password" />
+                <input className="p-2 border border-gray-300 rounded" type="email" placeholder="Email" 
+                value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input className="p-2 border border-gray-300 rounded" type="password" placeholder="Password" 
+                value={password} onChange={(e) => setPassword(e.target.value)}/>
                 <input className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600" type="submit" placeholder="Submit" />
             </form>
         </div>
